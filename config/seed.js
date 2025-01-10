f08a41167426404a58cac2b55ec669b65e8097cd
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-01-10 06:55:22 -0500

    updated: files renamed

--- config/seed.js
+++
@@ -1,5 +0,0 @@
-import skeletal_system_demo from './bone_seed.json' with {type: 'json'};
-import muscular_system_demo from './muscle_seed.json' with {type: 'json'};
-import physiology_demo from './physiology_seed.json' with {type: 'json'};
-
-export { muscular_system_demo, skeletal_system_demo, physiology_demo };
